2d17eadabf91a420d6adfc20512bf01701850152
Xavier Watkins <xavier.watkins@gmail.com>
2020-06-26 10:16:51 +0100

    Fix data result

--- src/components/ProteinContainer.tsx
+++ src/components/ProteinContainer.tsx
@@ -4,18 +4,18 @@ import ProteinCard, { ProteinData } from "./cards/ProteinCard";
 import { baseUrl } from "../config";
 
 const ProteinContainer: FunctionComponent<{ id: string }> = ({ id }) => {
-  const { data } = useApi<{ results: ProteinData }>(
-    `${baseUrl}/proteins/${id}`
-  );
+  const { data } = useApi<{ result: ProteinData }>(`${baseUrl}/proteins/${id}`);
+
   if (!data) {
     return null;
   }
+
   return (
     <Fragment>
       <div className="page-header">
         <h2>Protein</h2>
       </div>
-      <ProteinCard data={data?.results} id={id} />;
+      <ProteinCard data={data?.result} id={id} />;
     </Fragment>
   );
 };
